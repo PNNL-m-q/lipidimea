2a089e2322318fb335ae358f250f4f67b9b3385b
Ross, Dylan <dylan.ross@pnnl.gov>
2023-11-02 12:15:39 -0700

    added n_chains to fragmentation rules that contain FAs, also reworked the load_rules function in _fragmentation_rules.py module to work with the newly formatted rules, and slight changes to _FragRule and subclasses as well

--- lipidimea/lipids/_fragmentation_rules.py
+++ lipidimea/lipids/_fragmentation_rules.py
@@ -8,43 +8,37 @@ Dylan Ross (dylan.ross@pnnl.gov)
 
 
 from os import path as op
+from glob import glob
 
 import yaml
 from mzapy.isotopes import monoiso_mass
 
 
-# lipid classes with fragmentation rules defined
-_FRAG_RULE_CLASSES = [
-    'CE', 
-    'Cer', 'GlcCer', 'LacCer',
-    'PC', 'PE', 'PG', 'PS', 
-    'SM', 
-    'TG'
-]
-
-
 class _FragRule():
     
-    def __init__(self, label, ionization, lipid_class, rule, diagnostic, neutral_loss, n_chains, acyl_chain_type):
+    def __init__(self, 
+                 static, label, ionization, lmaps_prefix, rule, diagnostic, neutral_loss, n_chains):
+        self.static = static
         self.lbl = label
         self.ionization = ionization
-        self.lipid_class = lipid_class
+        self.lmaps_prefix = lmaps_prefix
         self.rule = rule
         self.diagnostic = diagnostic
         self.neutral_loss = neutral_loss
         self.n_chains = n_chains
-        acyl_chain_type = acyl_chain_type
     
-    def _mz(self, mz, pre_mz, d_label):
+    def _mz(self, 
+            mz, pre_mz, d_label):
         """ determine whether to treat this m/z as a neutral loss """
-        # self._mz() defined by subclass, computes the m/z 
+        # self.mz() defined by subclass, computes the m/z 
         d = monoiso_mass({'H': -d_label, 'D': d_label}) if d_label is not None else 0.
         if self.neutral_loss:
             return pre_mz - mz - d
         else:
             return mz + d
     
-    def _label(self, label):
+    def _label(self, 
+               label):
         """ add a * to diagnostic fragment labels """
         return '*' + label if self.diagnostic else label
         
@@ -52,26 +46,24 @@ class _FragRule():
 class _FragRuleStatic(_FragRule):
     
     def __init__(self, 
-                 label, ionization, lipid_class, rule,
-                 diagnostic=False, neutral_loss=False, n_chains=1, acyl_chain_type='standard',
-                 ):
+                 label, ionization, lmaps_prefix, rule,
+                 diagnostic=False, neutral_loss=False):
         """
         Parameters
         ----------
         label : ``str``
         ionization : ``str``
             ionization mode, 'POS' or 'NEG'
-        lipid_class : ``str``
-            lipid class abbreviation
-            TODO (Dylan Ross): this could later be adapted to use LipidMAPS classification
-                               system instead of simple lipid class abbreviation
+        lmaps_prefix : ``str``
+            LipidMAPS classification prefix
         rule : ``dict(str:int)``
             dictionary mapping element to count
         """
-        self.static = True
-        super().__init__(label, ionization, lipid_class, rule, diagnostic, neutral_loss, n_chains, acyl_chain_type)
+        super().__init__(True, label, ionization, lmaps_prefix, rule, diagnostic, neutral_loss, None)
     
-    def mz(self, pre_mz, d_label=None):
+    def mz(self, 
+           pre_mz, 
+           d_label=None):
         """ 
         Parameters
         ----------
@@ -88,23 +80,22 @@ class _FragRuleStatic(_FragRule):
 class _FragRuleDynamic(_FragRule):
     
     def __init__(self, 
-                 label, ionization, lipid_class, rule,
-                 diagnostic=False, neutral_loss=False, n_chains=1, acyl_chain_type='standard'):
+                 label, ionization, lmaps_prefix, rule, n_chains,
+                 diagnostic=False, neutral_loss=False):
         """
         Parameters
         ----------
         label : ``str``
         ionization : ``str``
             ionization mode, 'POS' or 'NEG'
-        lipid_class : ``str``
-            lipid class abbreviation
-            TODO (Dylan Ross): this could later be adapted to use LipidMAPS classification
-                               system instead of simple lipid class abbreviation
+        lmaps_prefix : ``str``
+            LipidMAPS classification prefix
         rule : ``dict(str:(str or int))``
             dictionary mapping element to static count (int) or dynamic rule (str) 
+        n_chains : ``int``
+            number of chains for fragmentation rule
         """
-        self.static = False
-        super().__init__(label, ionization, lipid_class, rule, diagnostic, neutral_loss, n_chains, acyl_chain_type)
+        super().__init__(False, label, ionization, lmaps_prefix, rule, diagnostic, neutral_loss, n_chains)
     
     def mz(self, pre_mz, c, u, d_label=None):
         """ 
@@ -129,14 +120,18 @@ class _FragRuleDynamic(_FragRule):
         return self._label(self.lbl.format(c=c, u=u))
     
     
-def load_rules(lipid_class, ionization):
+# see what classes have fragmentation rules defined
+_FRAG_RULE_CLASSES = [op.splitext(op.split(_)[-1])[0] for _ in glob(op.abspath(op.join(__file__, op.pardir, op.pardir, '_include/rules/LM*')))]
+
+
+def load_rules(lmaps_prefix, ionization):
     """
     Load all fragmentation rules relevant to a particular lipid class and ionization
 
     Parameters
     ----------
-    lipid_class : ``str``
-        lipid class (standard abbreviation)
+    lmaps_prefix : ``str``
+        LipidMAPS classification prefix
     ionization : ``str``
         "POS" or "NEG"
     
@@ -149,21 +144,21 @@ def load_rules(lipid_class, ionization):
     rules = []
     any_path = op.join(rule_dir, 'any.yml')
     with open(any_path, 'r')as yff:
-        rules_ = yaml.safe_load(yff)['ionization'][ionization]
-    if lipid_class in _FRAG_RULE_CLASSES:
-        yf_pth = op.join(rule_dir, '{}.yml'.format(lipid_class))
+        rules_ = yaml.safe_load(yff)[ionization]
+    if lmaps_prefix in _FRAG_RULE_CLASSES:
+        yf_pth = op.join(rule_dir, '{}.yml'.format(lmaps_prefix))
         with open(yf_pth, 'r') as yf:
-            rules_ += yaml.safe_load(yf)['ionization'][ionization]
+            rules_ += yaml.safe_load(yf)[ionization]
     for rule in rules_:
         nl = 'neutral_loss' in rule and rule['neutral_loss']
         diag = 'diagnostic' in rule and rule['diagnostic']
-        n_chains = rule['n_chains'] if 'n_chains' in rule else 1
+        n_chains = rule['n_chains'] if 'n_chains' in rule else None
         if 'static' in rule and not rule['static']:
-            rule = _FragRuleDynamic(rule['label'], ionization, lipid_class, rule['rule'],
-                                    diagnostic=diag, neutral_loss=nl, n_chains=n_chains)
+            rule = _FragRuleDynamic(rule['label'], ionization, lmaps_prefix, rule['rule'], n_chains,
+                                    diagnostic=diag, neutral_loss=nl)
         else:
-            rule = _FragRuleStatic(rule['label'], ionization, lipid_class, rule['rule'],
-                                   diagnostic=diag, neutral_loss=nl, n_chains=n_chains)
+            rule = _FragRuleStatic(rule['label'], ionization, lmaps_prefix, rule['rule'],
+                                   diagnostic=diag, neutral_loss=nl)
         rules.append(rule)
     return rules
 
