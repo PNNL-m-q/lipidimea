12ba1e9be88d6b02c7c0a5b145d904ac77ecef6a
Ross, Dylan <dylan.ross@pnnl.gov>
2024-02-19 15:14:31 -0800

    implement unit tests for lipidimea.msms._util module

--- lipidimea/msms/_util.py
+++ lipidimea/msms/_util.py
@@ -8,24 +8,63 @@ Dylan Ross (dylan.ross@pnnl.gov)
 
 
 import os
+import re
+from typing import Any, Callable, List, Dict
 
 import numpy as np
-            
+import numpy.typing as npt
 
-def ms2_to_str(mzs, iis):
+
+def ms2_to_str(mzs: npt.NDArray[np.float64], iis: npt.NDArray[np.float64]
+               ) -> str :
     """  
     converts arrays of m/z bins and intensities to flat str representation
+    with space-separated peaks in format "{mz}:{intensity}"
+    
+    Parameters
+    ----------
+    mzs : ``numpy.ndarray(float)``
+    iis : ``numpy.ndarray(float)``
+        m/z and intensity components of mass spectrum as arrays
+
+    Returns
+    -------
+    spec_string : ``str``
+        string representation of spectrum
     """
+    lm, li = len(mzs), len(iis)
+    if lm != li:
+        msg = f"ms2_to_str: mzs and iis arrays have different lengths ({lm} and {li})"
+        raise ValueError(msg)
+    if lm < 1:
+        msg = f"ms2_to_str: mzs and iis arrays should not be empty"
+        raise ValueError(msg)        
     s = ''
     for mz, i in zip(mzs, iis):
         s += '{:.4f}:{:.0f} '.format(mz, i)
     return s.rstrip()
 
 
-def str_to_ms2(s):
+def str_to_ms2(s: str
+               ) -> npt.NDArray[np.float64] :
     """
-    converts flat str representation to arrays of m/z and intensities
+    converts flat str representation (space-separated peaks in format "{mz}:{intensity}") 
+    to arrays of m/z and intensities
+
+    Parameters
+    ----------
+    s : ``str``
+        string form of spectrum
+
+    Returns
+    -------
+    spectrum : ``numpy.ndarray(float)``
+        spectrum as 2D array with m/z and intensity components
     """
+    # check the format of the spectrum string
+    if not re.match(r'^([0-9]+[.][0-9]*:[0-9]+[ ]*)+$', s):
+        msg = "str_to_ms2: spectrum string not properly formatted"
+        raise ValueError(msg)
     ms_, is_ = [], []
     if ' ' in s:
         for ms2pk in s.split():
@@ -40,7 +79,8 @@ def str_to_ms2(s):
     return np.array([ms_, is_])
 
 
-def apply_args_and_kwargs(fn, args, kwargs):
+def apply_args_and_kwargs(fn: Callable, args: List[Any], kwargs: Dict[Any, Any]
+                          ) -> Any:
     """ small helper that enables multiprocessing.Pool.starmap with kwargs """
     return fn(*args, **kwargs)
 
