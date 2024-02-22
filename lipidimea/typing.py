24d692f4a72e991802b0e1b829de5c3362e2d639
Ross, Dylan <dylan.ross@pnnl.gov>
2024-02-22 11:35:36 -0800

    add new typing module

---
+++ lipidimea/typing.py
@@ -0,0 +1,20 @@
+"""
+lipidimea/typing.py
+
+Dylan Ross (dylan.ross@pnnl.gov)
+
+    type annotations for use across this package
+"""
+
+
+from typing import Union, Tuple, Optional
+import sqlite3
+
+
+type ResultsDBPath = str
+type ResultsDBCursor = sqlite3.Cursor
+type DdaReader = Union[_MSMSReaderDDA, _MSMSReaderDDA_Cached]
+type DdaChromFeat = Tuple[float, float, float, float, float]
+type DdaQdata = Tuple[None, str, float, float, float, float, float, int, Optional[int], Optional[str]]
+
+
