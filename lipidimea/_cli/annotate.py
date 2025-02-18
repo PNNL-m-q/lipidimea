3bc371b3ad3935fe73d0f441039df0d957ca5fbd
dylanhross <dylan.ross@pnnl.gov>
2025-02-17 17:03:14 -0800

    implement CLI for annotating lipids

--- lipidimea/_cli/annotate.py
+++ lipidimea/_cli/annotate.py
@@ -8,6 +8,7 @@ Dylan Ross (dylan.ross@pnnl.gov)
 
 import argparse
 
+from lipidimea.params import AnnotationParams
 from lipidimea.annotation import (
     annotate_lipids
 )
@@ -19,20 +20,21 @@ ANNOTATE_DESCRIPTION = """
 
 
 def setup_annotate_subparser(parser: argparse.ArgumentParser):
-    """ set up the subparser for dia subcommand """
+    """ set up the subparser for annotate subcommand """
     parser.add_argument(
-        "PARAMETERS",
+        "PARAMS_CONFIG",
         help="parameter config file (.yaml)"
     )
     parser.add_argument(
         "RESULTS_DB",
         help="results database file (.db)"
     )
-    #assert False, "do we need anything else?"
 
 
 def annotate_run(args: argparse.Namespace):
     """ perform lipid annotation """
-    print(args)
-    assert False, "TODO"
+    # load the parameters
+    params = AnnotationParams.from_config(args.PARAMS_CONFIG)
+    # annotate lipids
+    _ = annotate_lipids(args.RESULTS_DB, params, debug_flag="text")
 
