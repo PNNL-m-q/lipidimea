9bec0dd42aff97241a1021972bc4c4f929b75794
dylanhross <dylan.ross@pnnl.gov>
2025-02-19 14:50:48 -0800

    bump version ahead of merge

--- lipidimea/__init__.py
+++ lipidimea/__init__.py
@@ -7,7 +7,7 @@ Dylan Ross (dylan.ross@pnnl.gov)
 
 
 # release.major_version.minor_version
-__version__ = '0.13.1'
+__version__ = '0.13.2'
 
 
 # TODO (Dylan Ross): When migrating to GitHub, convert TODOs across the package into issues.
