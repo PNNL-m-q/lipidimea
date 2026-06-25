6b2a52dafdfefe9af2ab8d26cdc7ac63aef67e5c
Dylan Ross <dylan.ross@pnnl.gov>
2026-06-25 09:41:45 -0700

    bump version for first release

--- src/lipidimea/__init__.py
+++ src/lipidimea/__init__.py
@@ -7,7 +7,7 @@ Dylan Ross (dylan.ross@pnnl.gov)
 
 
 # release.major_version.minor_version
-__version__ = '0.13.3'
+__version__ = '1.0.0'
 
 
 # TODO (Dylan Ross): When migrating to GitHub, convert TODOs across the package into issues.
