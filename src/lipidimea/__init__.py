013f175d51a3b98350bce69b241e50bfbfeece82
Dylan Ross <dylan.ross@pnnl.gov>
2026-08-11 14:07:12 -0700

    fix: required package data not packaged

--- src/lipidimea/__init__.py
+++ src/lipidimea/__init__.py
@@ -7,7 +7,7 @@ Dylan Ross (dylan.ross@pnnl.gov)
 
 
 # release.major_version.minor_version
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 
 
 # TODO (Dylan Ross): When migrating to GitHub, convert TODOs across the package into issues.
